ca2ec0393f651e84ae085ebc3cdacbd1d9fa7890
leopheard <leopheard.misc@gmail.com>
2020-03-29 23:03:02 -0400

    upload

--- addon.py
+++ addon.py
@@ -2,15 +2,10 @@ from xbmcswift2 import Plugin, xbmcgui
 from resources.lib import mainaddon
 
 plugin = Plugin()
-
-# base url for fetching podcasts 
 URL = "https://www.blogtalkradio.com/kylekulinski/podcast"
 
 @plugin.route('/')
 def main_menu():
-    """
-    main menu 
-    """
     items = [
         {
             'label': plugin.get_string(30000), 
@@ -25,20 +20,13 @@ def main_menu():
 
 @plugin.route('/all_episodes/')
 def all_episodes():
-    """
-    contains playable podcasts listed as just-in
-    """
     soup = mainaddon.get_soup(URL)
     playable_podcast = mainaddon.get_playable_podcast(soup)
     items = mainaddon.compile_playable_podcast(playable_podcast)
     return items
 
-
 @plugin.route('/all_episodes1/')
 def all_episodes1():
-    """
-    contains playable podcasts listed as just-in
-    """
     soup = mainaddon.get_soup(URL)
     playable_podcast1 = mainaddon.get_playable_podcast1(soup)
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
